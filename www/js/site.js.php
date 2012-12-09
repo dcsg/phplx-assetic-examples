8c6ad629d8b9ee28ff28a4ce6854f15d5d81b3a1
Daniel Gomes <me@danielcsgomes.com>
2012-12-09 12:53:09 +0000

    Added Asset Collection

--- www/js/site.js.php
+++ www/js/site.js.php
@@ -3,13 +3,21 @@
 require_once '../../vendor/autoload.php';
 
 use Assetic\Asset\FileAsset;
+use Assetic\Asset\AssetCollection;
+use Assetic\Asset\GlobAsset;
 use Assetic\Filter\Yui\JsCompressorFilter;
 $yuiFilter = new JsCompressorFilter('../../java/yuicompressor-2.4.7.jar');
 
-$jsAsset = new FileAsset(
-    '../javascripts/jquery-1.8.3.js',
+$jsAC = new AssetCollection(
+    array(
+         new FileAsset(
+             '../javascripts/jquery-1.8.3.js'
+         ),
+         new GlobAsset('../javascripts/twitter-bootstrap/*.js')
+    ),
     array($yuiFilter)
 );
 
+
 header('Content-Type: application/javascript');
-echo $jsAsset->dump();
+echo $jsAC->dump();
--- www/stylesheets/site.css.php
+++ www/stylesheets/site.css.php
@@ -3,16 +3,30 @@
 require_once '../../vendor/autoload.php';
 
 use Assetic\Asset\FileAsset;
+use Assetic\Asset\AssetCollection;
+use Assetic\Filter\LessFilter;
 use Assetic\Filter\Yui\CssCompressorFilter;
 use Assetic\Filter\CompassFilter;
 
-$yuiFilter = new CssCompressorFilter('../../java/yuicompressor-2.4.7.jar');
-$compassFilter = new CompassFilter();
+$yuiFilter     = new CssCompressorFilter('../../java/yuicompressor-2.4.7.jar');
+$compassFilter = new CompassFilter('/usr/bin/compass');
+$lessFilter    = new LessFilter('/usr/local/bin/node', array('/usr/local/lib/node_modules'));
 
-$cssAsset = new FileAsset(
-    '../sass/screen.scss',
-    array($compassFilter, $yuiFilter)
+$cssAC = new AssetCollection(
+    array(
+         new FileAsset(
+             '../sass/screen.scss',
+             array($compassFilter)
+         ),
+         new FileAsset(
+             '../less/twitter-bootstrap/bootstrap.less',
+             array($lessFilter)
+
+         )
+    ),
+    array($yuiFilter)
 );
 
+
 header('Content-Type: text/css');
-echo $cssAsset->dump();
+echo $cssAC->dump();
