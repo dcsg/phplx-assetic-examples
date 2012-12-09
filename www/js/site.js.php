67c984fd1c95e07d77fe56c876ea0b6ec5391c37
Daniel Gomes <me@danielcsgomes.com>
2012-12-09 13:08:47 +0000

    added asset & filter managers for css & js

--- www/js/site.js.php
+++ www/js/site.js.php
@@ -4,23 +4,26 @@ require_once '../../vendor/autoload.php';
 
 use Assetic\Asset\FileAsset;
 use Assetic\Asset\AssetCollection;
+use Assetic\AssetManager;
+use Assetic\FilterManager;
 use Assetic\Asset\GlobAsset;
 use Assetic\Filter\Yui\JsCompressorFilter;
 
-$yuiFilter = new JsCompressorFilter('../../java/yuicompressor-2.4.7.jar');
 
-$jsAC = new AssetCollection(
-    array(
-         new FileAsset(
-             '../javascripts/jquery-1.8.3.js'
-         ),
-         new AssetCollection(
-             array(new GlobAsset('../javascripts/twitter-bootstrap/*.js'))
-         )
-    ),
-    array($yuiFilter)
+$filterManager = new FilterManager();
+$filterManager->set('yui_js', new JsCompressorFilter('../../java/yuicompressor-2.4.7.jar'));
+
+$assetManager = new AssetManager();
+$assetManager->set(
+    'twitter_bootstrap',
+    new AssetCollection(
+        array(
+             new FileAsset('../javascripts/jquery-1.8.3.js'),
+             new GlobAsset('../javascripts/twitter-bootstrap/*.js')
+        ),
+        array($filterManager->get('yui_js'))
+    )
 );
 
-
 header('Content-Type: application/javascript');
-echo $jsAC->dump();
+echo $assetManager->get('twitter_bootstrap')->dump();
--- www/stylesheets/site.css.php
+++ www/stylesheets/site.css.php
@@ -4,29 +4,35 @@ require_once '../../vendor/autoload.php';
 
 use Assetic\Asset\FileAsset;
 use Assetic\Asset\AssetCollection;
+use Assetic\AssetManager;
+use Assetic\FilterManager;
 use Assetic\Filter\LessFilter;
 use Assetic\Filter\Yui\CssCompressorFilter;
 use Assetic\Filter\CompassFilter;
 
-$yuiFilter     = new CssCompressorFilter('../../java/yuicompressor-2.4.7.jar');
-$compassFilter = new CompassFilter('/usr/bin/compass');
-$lessFilter    = new LessFilter('/usr/local/bin/node', array('/usr/local/lib/node_modules'));
 
-$cssAC = new AssetCollection(
-    array(
-         new FileAsset(
-             '../sass/screen.scss',
-             array($compassFilter)
-         ),
-         new FileAsset(
-             '../less/twitter-bootstrap/bootstrap.less',
-             array($lessFilter)
+$filterManager = new FilterManager();
+$filterManager->set('yui_css', new CssCompressorFilter('../../java/yuicompressor-2.4.7.jar'));
+$filterManager->set('compass', new CompassFilter('/usr/bin/compass'));
+$filterManager->set('less', new LessFilter('/usr/local/bin/node', array('/usr/local/lib/node_modules')));
 
-         )
-    ),
-    array($yuiFilter)
+$assetManager = new AssetManager();
+$assetManager->set('3rd_party',
+    new AssetCollection(
+        array(
+             new FileAsset(
+                 '../sass/screen.scss',
+                 array($filterManager->get('compass'))
+             ),
+             new FileAsset(
+                 '../less/twitter-bootstrap/bootstrap.less',
+                 array($filterManager->get('less'))
+
+             )
+        ),
+        array($filterManager->get('yui_css'))
+    )
 );
 
-
 header('Content-Type: text/css');
-echo $cssAC->dump();
+echo $assetManager->get('3rd_party')->dump();
