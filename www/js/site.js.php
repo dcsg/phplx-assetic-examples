d0d3b7cfeeef7cc724199b903d022722fadad400
Daniel Gomes <me@danielcsgomes.com>
2012-12-09 13:11:56 +0000

    Added asset reference to javascript file

--- www/js/site.js.php
+++ www/js/site.js.php
@@ -4,6 +4,7 @@ require_once '../../vendor/autoload.php';
 
 use Assetic\Asset\FileAsset;
 use Assetic\Asset\AssetCollection;
+use Assetic\Asset\AssetReference;
 use Assetic\AssetManager;
 use Assetic\FilterManager;
 use Assetic\Asset\GlobAsset;
@@ -14,11 +15,20 @@ $filterManager = new FilterManager();
 $filterManager->set('yui_js', new JsCompressorFilter('../../java/yuicompressor-2.4.7.jar'));
 
 $assetManager = new AssetManager();
+
+// Sets the jquery AM
+$assetManager->set(
+    'jquery',
+    new FileAsset('../javascripts/jquery-1.8.3.js'),
+    array($filterManager->get('yui_js'))
+);
+
+// Sets the twitter bootstrap AM that will referencing the jquery AM
 $assetManager->set(
     'twitter_bootstrap',
     new AssetCollection(
         array(
-             new FileAsset('../javascripts/jquery-1.8.3.js'),
+             new AssetReference($assetManager, 'jquery'),
              new GlobAsset('../javascripts/twitter-bootstrap/*.js')
         ),
         array($filterManager->get('yui_js'))
